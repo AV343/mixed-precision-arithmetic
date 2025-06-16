5cab79a1c7a16ea3e1d8785fbc581dc8835334ab
AV343 <aniruddh.vasishta@gmail.com>
2025-06-16 12:51:00 -0400

    Updated header and TODO files

--- bigInt.h
+++ bigInt.h
@@ -3,18 +3,20 @@
 #include <vector>
 
 class bigInt{
-	public:
-		bigInt(std::vector<unsigned int> blocks, bool is_positive); // Array Constructor
-		bigInt(int k); // 0 Constructor
-		
+	private:
 		// Fields needed for arithmetic
 		std::vector<unsigned int> blocks;
 		bool is_positive;
 		int k;
 
+	public:
+		bigInt(std::vector<unsigned int> blocks, bool is_positive); // Array Constructor
+		bigInt(int k); // 0 Constructor
+
 		unsigned int get_block(int i, unsigned int if_fails=0);
 		bool set_block(int i, unsigned int n);
 		std::string toString();
+		void extend(bigInt a);
 		
 		friend bigInt operator<<(const bigInt& a, unsigned int n);
 		friend bigInt operator>>(const bigInt& a, unsigned int n);
